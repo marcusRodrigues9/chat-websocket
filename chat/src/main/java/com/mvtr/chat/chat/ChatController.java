bd515e8af9cdc5cae6f295c788f1e1197c6abb2b
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2026-02-14 12:33:11 -0300

    feat: adiciona controller de chat com suporte a salas dinâmicas via WebSocket

---
+++ chat/src/main/java/com/mvtr/chat/chat/ChatController.java
@@ -0,0 +1,19 @@
+package com.mvtr.chat.chat;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.send/{room}")
+    @SendTo("/topic/chat/{room}")
+    public ChatMessage sendToRoom(@DestinationVariable String room, ChatMessage message){
+        message.setTimestamp(LocalDateTime.now());
+        return message;
+    }
+}
