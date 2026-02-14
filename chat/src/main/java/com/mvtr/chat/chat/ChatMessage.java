06c2138a6acc2092132e1cc65d3a12b5dc13d18f
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2026-02-14 12:29:07 -0300

    feat: adiciona construtor// getters e setters

--- chat/src/main/java/com/mvtr/chat/chat/ChatMessage.java
+++ chat/src/main/java/com/mvtr/chat/chat/ChatMessage.java
@@ -1,4 +1,40 @@
 package com.mvtr.chat.chat;
 
+import java.time.LocalDateTime;
+
 public class ChatMessage {
+
+    private String from;
+    private String content;
+    private LocalDateTime timestamp;
+
+    public ChatMessage(String from, String content, LocalDateTime timestamp) {
+        this.from = from;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
